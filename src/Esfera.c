08b56bc1bda9cccbc9387b8ab3839720bec299a4
JuanFe <juanfelipeguerreroarias@gmail.com>
2025-03-20 14:54:19 -0500

    Termine las funciones de la esfera

--- src/Esfera.c
+++ src/Esfera.c
@@ -1,12 +1,21 @@
 #include "Esfera.h"
 #include "Raytracing.h"
 #include "Vector.h"
+#include <math.h>
 
 int hit(Esfera E, Rayo r, float *x ){
     Vector3  h = rest3(r.origen, E.centro);
     float a = dot3(r.origen,r.direccion);
     float b = 2.0 * dot3(h,r.direccion);
     float c = dot3(h,h) - E.r * E.r; 
+    float discriminante = b*b  - 4 * a * c;
 
-    
+    if (discriminante > 0){
+        *x = (-b - sqrt(discriminante))/(2.0*a);
+
+        return(*x > 0);
+    }
+
+    return 0;
+
 }
